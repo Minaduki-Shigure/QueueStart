2848402bd22adb8fd6a0ab6087f37dea094ed8b9
吴康正 <wukangzheng991025@foxmail.com>
2018-04-11 16:43:38 +0800

    Debug 0.2

--- QueueStart/main.c
+++ QueueStart/main.c
@@ -3,19 +3,19 @@
 int main(void)
 {
 	int size = 100;
-	char buffer = 0;
+	int buffer = 0;
 	puts("Creating the queue, please enter the size of the queue:");
 	scanf_s("%d", &size);
 	CircQueue Q = QueueInit(size);
-	puts("Initializing the queue, please input the elements of the queue one by one with a '$' as the end.");
+	puts("Initializing the queue, please input the elements of the queue one by one with a '-1' as the end.");
 	while (1)
 	{
-		buffer = _getche();
-		if (buffer == '$')
+		scanf_s("%d", &buffer);
+		if (buffer == -1)
 			break;
 		EnQueue(&Q, buffer);
 	}
-	puts("\nQueue sucessfully initialized!");
+	puts("Queue sucessfully initialized!");
 	QueuePrint(Q);
 	system("pause");
 	return 0;
