9dc65e350fac21e1091e3681eeec5211f607eeeb
吴康正 <wukangzheng991025@foxmail.com>
2018-04-11 16:54:46 +0800

    Debug 1.0

--- QueueStart/main.c
+++ QueueStart/main.c
@@ -2,21 +2,24 @@
 
 int main(void)
 {
-	int size = 100;
+	int n = 0;
+	int i = 0;
 	int buffer = 0;
-	puts("Creating the queue, please enter the size of the queue:");
-	scanf_s("%d", &size);
-	CircQueue Q = QueueInit(size);
-	puts("Initializing the queue, please input the elements of the queue one by one with a '-1' as the end.");
-	while (1)
+	puts("Please enter the value of n:");
+	scanf_s("%d", &n);
+	CircQueue Q1 = QueueInit(n + 1);
+	puts("Please input all the numbers one by one.");
+	while (i < n)
 	{
 		scanf_s("%d", &buffer);
-		if (buffer == -1)
-			break;
-		EnQueue(&Q, buffer);
+		EnQueue(&Q1, buffer);
+		i++;
 	}
-	puts("Queue sucessfully initialized!");
-	QueuePrint(Q);
+	puts("Numbers sucessfully readed!");
+	QueuePrint(Q1);
+	puts("Press any key to start the compare!");
+	system("pause");
+
 	system("pause");
 	return 0;
 }
